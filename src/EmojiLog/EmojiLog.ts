c7575024050f33a27de78b91c36d1f634d16d19c
Aashish <pawarashish564@gmail.com>
2020-07-20 14:06:52 +0530

    ✨ NEW: Adding new emojis

--- src/EmojiLog/EmojiLog.ts
+++ src/EmojiLog/EmojiLog.ts
@@ -5,7 +5,7 @@ interface Emoji {
 
 let EmojiLog: Array<Emoji> = [
 	{
-		emoji: '📦 NEW:',
+		emoji: '💥 NEW:',
 		description: 'Add something entirely new.',
 	},
 	{
@@ -32,5 +32,82 @@ let EmojiLog: Array<Emoji> = [
 		emoji: '‼️ BREAKING:',
 		description: 'Change that breaks previous versions.',
 	},
+	{
+		emoji:"🎉 Initial Commit:",
+		description:'initial commit ',
+	},	
+	{
+		emoji: "✨ New Feature:",
+		description: 'New Feature in the development',
+	},
+	{
+		emoji: "✅ Adding Tests:",
+		description: 'Adding tests',
+	},
+	{
+		emoji: "✔️ Make a test pass:",
+		description: 'defined tests passed ',
+	},
+	{
+		emoji:"⚡ General Update: ",
+		description:'general Update',
+	},
+	{
+		emoji:"🎨 Improved formatting:",
+		description:'improved formatting and structure',
+	},
+	{
+		emoji:"🔒 Security:",
+		description:'security features added',
+	},
+	{
+		emoji:"🔨 Refactor Code: ",
+		description:'refactoring code',
+	},
+	{
+		emoji:"➖ Removing Dependency: ",
+		description:'removing dependency from the project',
+	},
+	{
+		emoji:"➕ Adding Dependency: ",
+		description:'adding new dependency for the project',
+	},
+	{
+		emoji:"👌 Code review :",
+		description:'code review changes ',
+	},
+	{
+		emoji:"🚧 Work in Progress",
+		description:'working on specific issue/bug ',
+	},
+	{
+		emoji:"🚑 Critical Hotfix",
+		description:'critical Hotfix',
+	},
+	{
+		emoji:"🚀 Deploy: ",
+		description:'Deploying stuff',
+	},
+	{
+		emoji:"📈 tracking code: ",
+		description:'tracking code or analytics',
+	},
+	{
+		emoji:"⏪ Reverting : ",
+		description:'Reverting changes',
+	},
+	{
+		emoji:"🔀 Merging: ",
+		description:'Merging branches',
+	},
+	{
+		emoji:"💩 Needs Improvement: ",
+		description:'Bad Code / needs improv',
+	},
+	{
+		emoji:"📦 Configuration: ",
+		description:'Changes in config files (package.json, pom.xml) ',
+	}
+	
 ];
 export default EmojiLog;
